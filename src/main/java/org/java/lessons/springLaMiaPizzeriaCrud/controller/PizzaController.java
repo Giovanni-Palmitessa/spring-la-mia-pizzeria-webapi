23f50c01a5ce53f87c9c962d8f665471e4c2ae1d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-09 16:33:48 +0100

    add method index

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/PizzaController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/PizzaController.java
@@ -1,9 +1,14 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.controller;
 
+import org.java.lessons.springLaMiaPizzeriaCrud.model.Pizza;
 import org.java.lessons.springLaMiaPizzeriaCrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -11,4 +16,13 @@ public class PizzaController {
     // Attributi
     @Autowired
     private PizzaRepository pizzaRepository;
+
+    // Mapping che prende e ci mostra lista pizze
+    @GetMapping
+    public String index(Model model) {
+
+        List<Pizza> pizzaList = pizzaRepository.findAll();
+        model.addAttribute("pizzaList", pizzaList);
+        return "pizzas/index";
+    }
 }
