deee39140520d3a96d151cb74b507ba9b9a1e73b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-09 16:06:37 +0100

    add atributes

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Pizza.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Pizza.java
@@ -1,9 +1,22 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "pizzas")
 public class Pizza {
+    // Attributi
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Lob
+    private String description;
+    private String imageURL;
+    @Column(nullable = false)
+    private double price;
+    private LocalDateTime createdAt;
 }
