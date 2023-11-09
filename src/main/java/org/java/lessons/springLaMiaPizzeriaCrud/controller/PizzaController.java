2a93302d3b723b01ffb29f47c6c3ea2a5af8c170
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-09 16:32:17 +0100

    add pizzaRepository

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/PizzaController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/PizzaController.java
@@ -1,9 +1,14 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.controller;
 
+import org.java.lessons.springLaMiaPizzeriaCrud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/pizzas")
 public class PizzaController {
+    // Attributi
+    @Autowired
+    private PizzaRepository pizzaRepository;
 }
