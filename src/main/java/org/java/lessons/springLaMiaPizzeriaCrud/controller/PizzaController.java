9a63e279d8be89377422ef59e5e4d8b3c204bff0
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-10 14:15:20 +0100

    add mapping for show in controller

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/PizzaController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/PizzaController.java
@@ -3,12 +3,16 @@ package org.java.lessons.springLaMiaPizzeriaCrud.controller;
 import org.java.lessons.springLaMiaPizzeriaCrud.model.Pizza;
 import org.java.lessons.springLaMiaPizzeriaCrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -32,5 +36,21 @@ public class PizzaController {
         List<Pizza> pizzaList = pizzaRepository.findAll();
         model.addAttribute("pizzaList", pizzaList);
         return "pizzas/index";
+    }
+
+    // show che ci mostra i dettagli delle pizze
+    @GetMapping("/pizzas/show/{id}")
+    public String show(@PathVariable Integer id, Model model){
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        // verifico se il risultato è presente
+        if (result.isPresent()) {
+            // passo al template l'oggetto book
+            model.addAttribute("pizza", result.get());
+            return "pizzas/show";
+        } else {
+            // se non ho trovato il libro
+            // sollevo eccezione
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found!");
+        }
     }
 }
