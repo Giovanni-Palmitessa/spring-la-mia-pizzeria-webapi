844c8b2b059ac05a931a15485c7c3a27f58409e4
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-10 15:22:26 +0100

    add query for search in PizzaRepository

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/repository/PizzaRepository.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/repository/PizzaRepository.java
@@ -3,5 +3,9 @@ package org.java.lessons.springLaMiaPizzeriaCrud.repository;
 import org.java.lessons.springLaMiaPizzeriaCrud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PizzaRepository  extends JpaRepository<Pizza, Integer> {
+    // query custom per ricerca su title e authors
+    List<Pizza> findByNameContainingIgnoreCase(String nameKeyword);
 }
