59d07bfc431c5a482f7bd9d2b72da60cae6a127a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-13 14:18:53 +0100

    add button for create pizza in index

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/PizzaController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/PizzaController.java
@@ -24,6 +24,7 @@ public class PizzaController {
 
     @GetMapping
     public String home() {
+//        throw new RuntimeException("FORCE ERROR"); serve per forzare errori
         return "redirect:/home";
     }
 
