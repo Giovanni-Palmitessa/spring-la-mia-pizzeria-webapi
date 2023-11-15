c1b043a6dcec9857bb6e8b4ac5a3945f9ee52d37
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-15 14:42:29 +0100

    add attributes

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Offer.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Offer.java
@@ -1,9 +1,22 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "offers")
 public class Offer {
+    // Attributi
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    private LocalDate startDate;
+    @NotNull
+    private LocalDate endDate;
+    @NotNull
+    private String title;
 }
