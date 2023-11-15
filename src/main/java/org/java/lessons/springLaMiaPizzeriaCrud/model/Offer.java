6facd64fe85688d5f4c95c526c40c514ed7e9996
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-15 14:40:27 +0100

    entity Offer

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Offer.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Offer.java
@@ -1,4 +1,9 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "offers")
 public class Offer {
 }
