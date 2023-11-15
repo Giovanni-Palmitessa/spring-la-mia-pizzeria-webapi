b005bccaf87caea369f72345f7f8e28c869a4748
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-15 15:14:32 +0100

    add form for offers

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/OfferController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/OfferController.java
@@ -1,14 +1,16 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/offers")
 public class OfferController {
     @GetMapping("/create")
-    public String create() {
+    public String create(@RequestParam Integer pizzaId, Model model) {
         return "offers/form";
     }
 }
