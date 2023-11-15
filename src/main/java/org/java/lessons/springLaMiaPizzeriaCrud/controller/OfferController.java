1d509b982b096600a5bc6f5ae709baba4d323aff
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-15 16:05:10 +0100

    add method store

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/OfferController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/OfferController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -31,5 +32,10 @@ public class OfferController {
         offer.setPizza(pizza);
         model.addAttribute("offer", offer);
         return "offers/form";
+    }
+
+    @PostMapping("/create")
+    public String store(Offer formOffer){
+
     }
 }
