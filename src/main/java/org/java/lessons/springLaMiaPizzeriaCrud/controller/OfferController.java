c16e49695e7d1d113579b2cb76e8d25bcf00f86d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-15 14:57:33 +0100

    add method create

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/OfferController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/OfferController.java
@@ -1,4 +1,14 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/offers")
 public class OfferController {
+    @GetMapping("/create")
+    public String create() {
+        return "offers/form";
+    }
 }
