dae47b0a54bc30a5aebfc71b9f9a0733bf34925d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-17 17:45:19 +0100

    add attribute Pizza List and n to n with ingred.

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Ingredient.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Ingredient.java
@@ -3,6 +3,10 @@ package org.java.lessons.springLaMiaPizzeriaCrud.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.List;
 
 @Entity
 @Table(name = "ingredients")
@@ -15,6 +19,10 @@ public class Ingredient {
     @NotBlank
     @Size(max = 50)
     private String name;
+
+    @ManyToMany(mappedBy = "ingredients")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<Pizza> pizzas;
 
     public Integer getId() {
         return id;
@@ -30,5 +38,13 @@ public class Ingredient {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Pizza> getPizzas() {
+        return pizzas;
+    }
+
+    public void setPizzas(List<Pizza> pizzas) {
+        this.pizzas = pizzas;
     }
 }
