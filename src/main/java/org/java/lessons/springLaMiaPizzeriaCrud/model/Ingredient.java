879901738adc6a675378b9ac35d7976b336e53e5
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-17 14:44:42 +0100

    add entity Ingredient

---
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Ingredient.java
@@ -0,0 +1,9 @@
+package org.java.lessons.springLaMiaPizzeriaCrud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ingredients")
+public class Ingredient {
+}
