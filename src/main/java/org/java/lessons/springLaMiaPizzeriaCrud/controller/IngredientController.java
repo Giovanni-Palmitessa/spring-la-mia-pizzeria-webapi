54b4539d13adf7bb0d6b23a5788da215f1ce6ff5
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-17 15:57:37 +0100

    add bindig result to refresh page

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/IngredientController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/IngredientController.java
@@ -1,11 +1,15 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.controller;
 
+import jakarta.validation.Valid;
 import org.java.lessons.springLaMiaPizzeriaCrud.model.Ingredient;
 import org.java.lessons.springLaMiaPizzeriaCrud.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IngredientController {
     @Autowired
     IngredientRepository ingredientRepository;
+
+    // metodo che mostra la pagina degli ingredienti
     @GetMapping
     public String index(Model model){
         model.addAttribute("ingredientList", ingredientRepository.findAll());
@@ -20,4 +26,17 @@ public class IngredientController {
         model.addAttribute("ingredientObj", new Ingredient());
         return "ingredients/index";
     }
+
+    // metodo che salva gli ingredienti
+    @PostMapping
+    public String save(@Valid @ModelAttribute("ingredientObj") Ingredient ingredient, BindingResult bindingResult,
+                       Model model) {
+        // testo che la categoria è valida
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("ingredientList", ingredientRepository.findAll());
+            return "ingredients/index";
+        }
+
+    }
+
 }
