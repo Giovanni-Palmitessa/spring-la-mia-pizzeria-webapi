aaddeb06399c602ab601b518954671f9d5aba404
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-17 14:46:25 +0100

    add attributes getter and setters

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Ingredient.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Ingredient.java
@@ -1,9 +1,34 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "ingredients")
 public class Ingredient {
+    // Attributi
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
