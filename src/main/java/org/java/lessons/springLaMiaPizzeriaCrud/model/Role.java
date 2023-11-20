d166ccc4824535ec4d4267caa946e34c0c0e4960
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-20 14:33:03 +0100

    add attributes getter and setter in Role class

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Role.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Role.java
@@ -1,9 +1,32 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "roles")
 public class Role {
+    // Attributi
+    @Id
+    private Integer id;
+    private String name;
+
+    // Getter e Setter
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
