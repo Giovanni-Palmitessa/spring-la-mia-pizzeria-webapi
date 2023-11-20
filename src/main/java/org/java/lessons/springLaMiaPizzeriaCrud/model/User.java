311bf076d93759c13a448023a5e67ced3b86a2ec
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-20 14:22:57 +0100

    add attributes

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/User.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/User.java
@@ -1,9 +1,25 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
 public class User {
+    // Atrributi
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    @Email
+    private String email;
+    private LocalDateTime registeredAt;
+    private String password;
 }
