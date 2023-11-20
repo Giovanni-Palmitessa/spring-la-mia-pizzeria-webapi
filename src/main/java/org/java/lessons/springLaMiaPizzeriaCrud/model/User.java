56a25cabe754d0aa1c8df6a5a71d3dd634ca63bd
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-20 14:23:38 +0100

    add getter and setter

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/User.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/User.java
@@ -22,4 +22,53 @@ public class User {
     private String email;
     private LocalDateTime registeredAt;
     private String password;
+
+    // Getter e setter
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getRegisteredAt() {
+        return registeredAt;
+    }
+
+    public void setRegisteredAt(LocalDateTime registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
