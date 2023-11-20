1545d07fe5263895c09e061e8691271515e4b521
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-20 17:08:59 +0100

    add authentication in UserController

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/UserController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/UserController.java
@@ -1,6 +1,9 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.controller;
 
+import org.java.lessons.springLaMiaPizzeriaCrud.security.DatabaseUserDetails;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/users")
 public class UserController {
     @GetMapping
-    public String index() {
+    public String index(Authentication authentication, Model model) {
+        DatabaseUserDetails principal = (DatabaseUserDetails) authentication.getPrincipal();
         return "users/index";
     }
 }
