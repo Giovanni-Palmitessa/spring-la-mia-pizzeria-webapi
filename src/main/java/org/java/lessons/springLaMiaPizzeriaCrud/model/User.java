b102ced461835433b17e0e09bee97e214921816c
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-20 14:21:07 +0100

    add entity user with table name users

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/User.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/User.java
@@ -1,7 +1,9 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 }
