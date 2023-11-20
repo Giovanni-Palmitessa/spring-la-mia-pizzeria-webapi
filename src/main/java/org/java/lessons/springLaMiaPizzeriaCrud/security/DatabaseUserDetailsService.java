422da186e61c4adc09e2209196a3b6803853fe7d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-20 15:16:08 +0100

    imoplemnet method of loadUserByUsername

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/security/DatabaseUserDetailsService.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/security/DatabaseUserDetailsService.java
@@ -1,5 +1,6 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.security;
 
+import org.java.lessons.springLaMiaPizzeriaCrud.model.User;
 import org.java.lessons.springLaMiaPizzeriaCrud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,12 +8,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class DatabaseUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        // prendo lo username che viene dal form del login ecerco su db un user con quella email
+        Optional<User> loggedUser = userRepository.findByEmail(username);
+        // se logged user è presente, c'è utente con quella email
+        if (loggedUser.isPresent()){
+            return new DatabaseUserDetails(loggedUser.get());
+        } else {
+            // se non è presente tiro un eccezione
+            throw new UsernameNotFoundException(username);
+        }
     }
 }
