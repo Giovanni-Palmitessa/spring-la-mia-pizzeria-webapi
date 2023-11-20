95a1ebde3a14047fd682d0285913be84a5ccae0c
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-20 15:14:42 +0100

    add method in userRepo

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/repository/UserRepository.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/repository/UserRepository.java
@@ -3,5 +3,8 @@ package org.java.lessons.springLaMiaPizzeriaCrud.repository;
 import org.java.lessons.springLaMiaPizzeriaCrud.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    public Optional<User> findByEmail(String email);
 }
