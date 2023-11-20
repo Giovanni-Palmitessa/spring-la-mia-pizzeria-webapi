9050582914f1e2b9f4b97d6fb4bedf6212d463de
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-20 17:11:52 +0100

    autowired userRepository

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/UserController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/controller/UserController.java
@@ -1,6 +1,9 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.controller;
 
+import org.java.lessons.springLaMiaPizzeriaCrud.model.User;
+import org.java.lessons.springLaMiaPizzeriaCrud.repository.UserRepository;
 import org.java.lessons.springLaMiaPizzeriaCrud.security.DatabaseUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
     @GetMapping
     public String index(Authentication authentication, Model model) {
         DatabaseUserDetails principal = (DatabaseUserDetails) authentication.getPrincipal();
+        User loggedUser = userRepository.findById(principal.getId()).get();
+        model.addAttribute(loggedUser.getFirstName());
+        model.addAttribute(loggedUser.getLastName());
+        // recupero la lista di users e la passo al model
         return "users/index";
     }
 }
