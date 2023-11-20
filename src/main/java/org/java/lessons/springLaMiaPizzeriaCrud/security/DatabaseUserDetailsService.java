09ad51a49c4effd7115cfb29fa88aa979d1b0c5d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-20 15:13:19 +0100

    autowired on userRepository

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/security/DatabaseUserDetailsService.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/security/DatabaseUserDetailsService.java
@@ -1,5 +1,7 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.security;
 
+import org.java.lessons.springLaMiaPizzeriaCrud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DatabaseUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
