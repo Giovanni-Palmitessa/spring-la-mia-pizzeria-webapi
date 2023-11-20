fabe457f0121e4908483fbfba86bd932a2a22618
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-20 14:31:46 +0100

    add Role class

---
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/model/Role.java
@@ -0,0 +1,9 @@
+package org.java.lessons.springLaMiaPizzeriaCrud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+}
