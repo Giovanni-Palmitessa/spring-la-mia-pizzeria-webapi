036d2e84bca985c1e2d1e31e4397115b7e10cf32
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-21 14:14:38 +0100

    autowire ann. on pizza Repo

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/api/PizzaRestController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/api/PizzaRestController.java
@@ -1,5 +1,7 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.api;
 
+import org.java.lessons.springLaMiaPizzeriaCrud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/pizzas")
 @CrossOrigin
 public class PizzaRestController {
+    @Autowired
+    PizzaRepository pizzaRepository;
 }
