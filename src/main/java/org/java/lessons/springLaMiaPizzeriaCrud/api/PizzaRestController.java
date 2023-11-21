dc97003d41e5f3c13e879794bd645730e7ee0707
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-11-21 14:12:24 +0100

    add rest controller annotation

--- src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/api/PizzaRestController.java
+++ src/main/java/org/java/lessons/springLaMiaPizzeriaCrud/api/PizzaRestController.java
@@ -1,4 +1,11 @@
 package org.java.lessons.springLaMiaPizzeriaCrud.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/pizzas")
+@CrossOrigin
 public class PizzaRestController {
 }
